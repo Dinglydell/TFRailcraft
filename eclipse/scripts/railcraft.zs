7f962df6d607f39f51004e687f6dff40d44b0b0f
Dinglydell <mrsplodgey@gmail.com>
2019-06-10 04:15:18 +0100

    Added some recipes and created hack fix for use of fresh water for RC stuff

--- eclipse/scripts/railcraft.zs
+++ eclipse/scripts/railcraft.zs
@@ -12,3 +12,55 @@ for lumber in <ore:woodLumber>.items {
 recipes.remove(<Railcraft:part.tie:1>);
 # rocks seem a little odd but what else could it be?
 recipes.addShaped(<Railcraft:part.tie:1>, [[null, <Railcraft:part.rebar>, null], [<ore:itemRock>, <ore:itemRock>, <ore:itemRock>]]);
+
+
+# logs -> charcoal
+# mods.Railcraft.CokeOven.addRecipe(<terrafirmacraft:item.coal:1>, <liquid:creosote> * 250, <ore:logWood>, 1800); 
+# above threw an error, not sure why but I'm doing this in code instead
+
+var fireBrick = <terrafirmacraft:item.Fire Brick:1>;
+var mortar = <terrafirmacraft:item.Mortar>;
+
+# coke oven
+recipes.addShaped(<Railcraft:machine.alpha:7>, [[fireBrick, mortar, fireBrick], [mortar, null, mortar],[fireBrick, mortar, fireBrick]]);
+
+
+var steel = <terrafirmacraft:item.Steel Ingot>;
+var blacksteel = <terrafirmacraft:item.Black Steel Ingot>;
+# Anvil recipes
+#standard rail
+mods.Terrafirmacraft.Anvil.addAnvilRecipe(<Railcraft:part.rail:0> * 2, steel, "rail", 4);
+game.setLocalization("en_US", "gui.plans.rail", "Rail");
+
+# advanced rail
+mods.Terrafirmacraft.Anvil.addAnvilRecipe(<Railcraft:part.rail:4>, blacksteel, "rail", 4);
+
+# Gears
+recipes.remove(<Railcraft:part.gear>);
+recipes.remove(<Railcraft:part.gear:1>);
+recipes.remove(<Railcraft:part.gear:2>);
+recipes.remove(<Railcraft:part.gear:3>);
+game.setLocalization("en_US", "gui.plans.gear", "Gear");
+
+# Gold
+mods.Terrafirmacraft.Anvil.addAnvilRecipe(<Railcraft:part.gear:0>, <terrafirmacraft:item.Gold Double Sheet>, "gear", 1);
+
+# Iron
+mods.Terrafirmacraft.Anvil.addAnvilRecipe(<Railcraft:part.gear:1>, <terrafirmacraft:item.Wrought Iron Double Sheet>, "gear", 3);
+
+# Steel
+mods.Terrafirmacraft.Anvil.addAnvilRecipe(<Railcraft:part.gear:2>, <terrafirmacraft:item.Steel Double Sheet>, "gear", 4);
+
+# Boiler tanks
+
+#Ore dict RC plates with TFC sheets
+<ore:plateIron>.add(<Railcraft:part.plate:0>);
+<ore:plateSteel>.add(<Railcraft:part.plate:1>);
+<ore:plateTin>.add(<Railcraft:part.plate:2>);
+
+# rolling machine - evil recipe
+<ore:plateTierSix>.add(<terrafirmacraft:item.Red Steel Sheet>);
+<ore:plateTierSix>.add(<terrafirmacraft:item.Blue Steel Sheet>);
+var rollingMachine = <Railcraft:machine.alpha:8>;
+recipes.remove(rollingMachine);
+recipes.addShaped(rollingMachine, [[<ore:plateTierSix>, <minecraft:piston>, <ore:plateTierSix>], [<minecraft:piston>, <ore:craftingTableWood>, <minecraft:piston>], [<ore:plateTierSix>, <minecraft:piston>, <ore:plateTierSix>]]);
--- src/main/java/dinglydell/TFRailcraft/TFRailcraft.java
+++ src/main/java/dinglydell/TFRailcraft/TFRailcraft.java
@@ -1,6 +1,11 @@
 package dinglydell.TFRailcraft;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 import com.bioxx.tfc.Items.Tools.ItemCustomBucket;
+import com.bioxx.tfc.api.TFCBlocks;
+import com.bioxx.tfc.api.TFCFluids;
 import com.bioxx.tfc.api.TFCItems;
 
 import cpw.mods.fml.common.Mod;
@@ -11,9 +16,15 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import dinglydell.TFRailcraft.item.TFRItems;
 import mods.railcraft.common.fluids.Fluids;
 import mods.railcraft.common.fluids.RailcraftFluids;
+import mods.railcraft.common.items.RailcraftItem;
+import mods.railcraft.common.util.crafting.CokeOvenCraftingManager;
+import mods.railcraft.common.util.crafting.RollingMachineCraftingManager;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 
 @Mod(modid = TFRailcraft.MODID, version = TFRailcraft.VERSION, dependencies="required-after:terrafirmacraft")//;required-after:railcraft")
 public class TFRailcraft
@@ -23,13 +34,26 @@ public class TFRailcraft
     
     @EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-
+    	replaceWater();
     }
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
     	registerItems();
+    	addRCRecipes();
     }
+	private void addRCRecipes() {
+		// logs -> charcoal
+		CokeOvenCraftingManager.getInstance().addRecipe(new ItemStack(TFCItems.logs), false, false, new ItemStack(TFCItems.coal, 1, 1), new FluidStack(Fluids.CREOSOTE.get(), 250), 1800);
+		// coal -> coke. maybe you want coke for steam engines?
+		//CokeOvenCraftingManager.getInstance().addRecipe(new ItemStack(TFCItems.coal, 1, 0), true, false, new ItemStack(mods.railcraft.common.items., 1, 1), new FluidStack(Fluids.CREOSOTE.get(), 250), 1800);
+		
+		
+		RollingMachineCraftingManager.getInstance().addRecipe(RailcraftItem.rail.getStack(8, 4), new Object[] { "s s", "s s", "s s", 's', TFCItems.blackSteelIngot});
+		
+		RollingMachineCraftingManager.getInstance().addRecipe(RailcraftItem.rail.getStack(8, 1), new Object[] { "trg", "trg", "trg", 't', RailcraftItem.rail.getStack(1, 0), 'r', Items.redstone, 'g', TFCItems.goldIngot});
+		
+	}
 	private void registerItems() {
 		TFRItems.woodenBucketCreosote = new ItemStack(new ItemCustomBucket(
 				Blocks.air).setUnlocalizedName("woodenBucketCreosote"), 1);
@@ -40,6 +64,35 @@ public class TFRailcraft
 				TFRItems.woodenBucketCreosote,
 				new ItemStack(TFCItems.woodenBucketEmpty));
 		
+	}
+	
+	private void replaceWater() {
+
+		try { // this, as you may well have noticed, is a bit of a hack
+			Field water = FluidRegistry.class.getDeclaredField("WATER");
+			finalField(water);
+			water.set(null, TFCFluids.FRESHWATER);
+
+			Field tag = Fluids.class.getDeclaredField("tag");
+			tag.setAccessible(true);
+			finalField(tag);
+			tag.set(Fluids.WATER, "freshwater");
+
+			Field waterBlock = Blocks.class.getDeclaredField("water");
+			finalField(waterBlock);
+			waterBlock.set(null, TFCBlocks.freshWaterStationary);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+	private void finalField(Field field) throws Exception {
+		Field modifiers = Field.class.getDeclaredField("modifiers");
+		modifiers.setAccessible(true);
+		modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
 	}
 }
     
